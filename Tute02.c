bf69e2deb56dfa0cc95b3a3931c7e15394941f9e
Lakshani Erandika <99162501+IT21354656@users.noreply.github.com>
2022-02-22 15:45:56 +0000

    Tute 02

--- Tute02.c
+++ Tute02.c
@@ -18,7 +18,22 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 #include <stdio.h>
 
-int main() {
+int main() 
+{
+  int dis, amou;
+   
+   printf("Input Distance : ");
+   scanf("%d", &dis);
+   
+   if(dis<30)
+   {
+       amou = 50*dis;
+   }
+   else if(dis>30)
+   {
+       amou = (50*30)+40*(dis-30);
+   }
+   printf("Amount is : %d", amou);
   
   return 0;
 }
